6cd46440aa49730e27368e9769c1a8b7554cb0f2
David Larlet <larlet@gmail.com>
2009-02-02 13:55:41 +0100

    Configure admin for the example and add a .hgignore, thanks Ariel Nunez

--- oauth_examples/provider/settings.py
+++ oauth_examples/provider/settings.py
@@ -31,4 +31,5 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.admin',
 )
--- oauth_examples/provider/urls.py
+++ oauth_examples/provider/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls.defaults import *
+from django.contrib import admin
 
 from oauth_provider.views import protected_resource_example
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^oauth/', include('oauth_provider.urls')),
     url(r'^oauth/photo/$', protected_resource_example, name='oauth_example'),
+    (r'^admin/(.*)', admin.site.root),
 )
