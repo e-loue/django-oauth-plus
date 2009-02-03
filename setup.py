bd4556dcd663f78aaa50d247e198e8e2d7604d52
David Larlet <larlet@gmail.com>
2009-02-04 00:32:38 +0100

    Add a setup.py file to ease setuptools installation, thanks Ariel Nunez

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='django-oauth',
+    version='0.1',
+    description='Support of OAuth in Django.',
+    author='David Larlet',
+    author_email='david@larlet.fr',
+    url='http://code.welldev.org/django-oauth/',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    # Make setuptools include all data files under version control,
+    # svn and CVS by default
+    include_package_data=True,
+    zip_safe=False,
+    # Tells setuptools to download setuptools_git before running setup.py so
+    # it can find the data files under Hg version control.
+    setup_requires=['hg.setuptools'],
+)
