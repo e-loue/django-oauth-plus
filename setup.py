a37f9ba42bbd3fa4b6a08184313c89b92928d662
David Larlet <larlet@gmail.com>
2009-10-21 23:44:43 +0200

    This is the time for a 1.1 release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     name='django-oauth',
-    version='1.0',
+    version='1.1',
     description='Support of OAuth in Django.',
     author='David Larlet',
     author_email='david@larlet.fr',
